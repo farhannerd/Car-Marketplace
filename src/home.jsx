d7bac894e45b7d445cb2aa6090e86f953d63471c
Farhan Akhtar <gitfarhan@gmail.com>
2025-02-17 20:09:46 +0530

    Footer and Information Section

--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Category from './components/Category'
 import MostSearchedCar from './components/MostSearchedCar'
+import InfoSection from './components/InfoSection'
+import Footer from './components/Footer'
 
 const home = () => {
   return (
@@ -18,6 +20,13 @@ const home = () => {
 
       {/* Most Searched Cars */}
       <MostSearchedCar />
+
+      {/* Information Section */}
+      <InfoSection/>
+
+      {/* Footer */}
+      <Footer />
+
     </div>
   )
 }
